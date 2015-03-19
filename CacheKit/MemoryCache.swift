35b5a826c3369b6f8b0a4950a78a135f69f59005
questbeat <questbeat@gmail.com>
2015-03-19 21:28:25 +0900

    Refactoring

--- CacheKit/MemoryCache.swift
+++ CacheKit/MemoryCache.swift
@@ -6,10 +6,10 @@
 //  Copyright (c) 2015年 Katsuma Tanaka. All rights reserved.
 //
 
+    import Foundation
+
 #if os(iOS)
     import UIKit
-#elseif os(OSX)
-    import Foundation
 #endif
 
 public class MemoryCache<T: AnyObject>: Cache {
